baac3294d3174487bc46a71bc9ddda61e41c1ef0
unknown <aminmirzoev123@gmail.com>
2020-05-01 21:02:04 +0500

    Class Customer

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace HW15_new
+{
+    class Program
+    {
+     public static List<Customer> CustomerList = new List<Customer>();
+        static void Main(string[] args)
+        {
+         
+        }
+        
+    }
+    class Customer
+    {
+        public int id {get;set;}
+        public decimal Balance{ get; set; }
+
+        public Customer(int id, decimal Balance)
+        {
+            this.id=id;
+            this.Balance=Balance;
+        }
+        public Customer()
+        {
+
+        }
+    }
+}
